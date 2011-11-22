145ad8b745b101e21085d6e75859bd37f99dcef2
Xavier Decuyper <xavier.decuyper@gmail.com>
2011-11-22 13:52:01 +0100

    Stijl toegevoegd voor knoppen welkomscherm

--- abc.php
+++ abc.php
@@ -10,17 +10,28 @@ include("conf/sessionCheck.php");
 </head>
 <body>
 	<div id="appBox">
-		<div id="logoSmall"></div>
+		<div id="logoSmall"> </div>
 
 		<div id="loggedInAs">
 			Welkom <?php echo $username ?><br />
 			<a href="logout.php">Uitloggen</a>
 		</div>
 
-		<div id="clear"></div>
+		<div style="clear:both;"> </div>
 
+		<ul id="selectieLeerkrachten">
 
+			<li><img src="img/pencil.png">
+				<a href="wensen.php">Wensen invoeren</a>
+			</li>
+			
+			<li><img src="img/klassen.png">
+				<a href="lokalen.php">Lokalen bekijken</a>
+			</li>
 
+		</ul>
+
+		<div id="clear"> </div>
 	</div>
 
 	<div id="footer">Copyright 2011, Xavier Decuyper</div>
