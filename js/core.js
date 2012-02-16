68c0bdb920af4697478a2b5bd39f5a081710b7e1
Xavier Decuyper <xavier.decuyper@gmail.com>
2012-02-16 13:53:41 +0100

    Overlay sluiten = pagina herladen. Fixt bugs met wijzigen. Closes #33

--- js/core.js
+++ js/core.js
@@ -17,16 +17,16 @@ function $(a){
 // afhankelijk van de huidige toestand
 //
 function toggleShade(){
-	
+
 	// Shade injecteren als deze nog niet bestaat
 	if( $('shade') == null ){
 		var shade = document.createElement('div');
 		shade.setAttribute('id', 'shade');
-		shade.setAttribute('onclick', 'toggleShade(); calendar.hideCalendar();');
-		
+		shade.setAttribute('onclick', 'window.location.reload(); calendar.hideCalendar();');
+
 		document.body.appendChild(shade); // element in body schrijven
 	}
-	
+
 	if( $('shade').style.display == 'block'){
 		$('shade').style.display = "none";
 		$('overlay').style.display = "none";
@@ -40,22 +40,22 @@ function toggleShade(){
 // Functie voor het valideren van een formulier
 //
 validate.form = function(formName){
-	
+
 	for(i=0; i<= formName.length -1; i++){
-		
+
 		if(formName[i].className.indexOf("required") != -1){
-			
+
 			var string = formName[i].value;
-			
+
 			if(string.replace(" ", "") == ""){
 				alert('Gelieve alle velden in te vullen.');
 				return false;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	// Alles is in orde. Formulier mag door, submit deactiveren
 	toggleSubmitbtn(formName);
 }
@@ -68,10 +68,10 @@ function toggleSubmitbtn(formName){
 
 	for(i=0; i<= formName.length -1; i++){
 		if(formName[i].type == "submit"){
-			
+
 			formName[i].value = "Bezig met laden...";
 			formName[i].disabled = true;
-			
-		}	
+
+		}
 	}
 }
