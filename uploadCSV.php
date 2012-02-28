5cd6671cad582e4508306e12b397c038d987848e
Xavier Decuyper <xavier.decuyper@gmail.com>
2012-02-28 15:22:37 +0100

    Detectie lijneindes aangepast. Controle voor lege lijn. Closes #35

--- uploadCSV.php
+++ uploadCSV.php
@@ -22,6 +22,51 @@ if(filesize($_FILES['file']['tmp_name']) > $max_filesize){
 
 $input = file_get_contents($_FILES['file']['tmp_name']);
 
+//
+// Verwijder ALLE spaties (Voor veiligheid)
+//
+$input = str_replace(' ', '', $input);
+
+//
+// Door de CSV lijnen lopen. Gebruikt automatisch de gebruikte line-endings
+// bron: http://stackoverflow.com/questions/1462720/iterate-over-each-line-in-a-string-in-php
+//
+foreach(preg_split("/(\r?\n)/", $input) as $line)
+{
+    $velden = explode(";", $line);
+	$username = $velden[0];
+	$pass = sha1($velden[1]);
+
+	//
+	// Excel heeft de neiging om een lege lijn aan het einde van CSV toe te voegen.
+	// dit is geen gebruiker en mag dus niet geimporteerd worden!
+	//
+	if($line == "" || $line == " ")
+	{
+		continue;
+	}
+
+	$qry_insert = mysql_query("INSERT INTO users (username, userpass, time_lastlogon, is_admin, active) VALUES ('$username', '$pass', '0000-00-00 00:00:00', '0', '1')");
+
+	if(!$qry_insert)
+	{
+		$error = mysql_errno($link);
+
+		if($error == "1062")
+		{
+			echo "<br> Gebruiker $username bestaat al! Niet geimporteerd.";
+			continue;
+		}
+		else
+		{
+			die("Er heeft zich een fout voorgedaan.");
+		}
+	}
+
+	echo "<br>Gebruiker $username toegevoegd.";
+}
+
+/*
 $rijen = explode("\r", $input);
 
 for($i = 0; $i<= count($rijen) -1; $i++){
@@ -43,7 +88,7 @@ for($i = 0; $i<= count($rijen) -1; $i++){
 	}
 
 	echo "<br>Gebruiker $username toegevoegd.";
-}
+}*/
 
 echo "<br><br> Importeren voltooid.<br>";
 echo "<a href='abc.php'>Klik hier om terug te keren</a><br>";
